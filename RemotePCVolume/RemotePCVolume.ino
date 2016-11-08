3273e424f3df01b6d06304795a4f237e30ab81c7
magusbr <magusbr@gmail.com>
2016-11-08 21:08:22 -0200

    Better performance

--- RemotePCVolume/RemotePCVolume.ino
+++ RemotePCVolume/RemotePCVolume.ino
@@ -41,7 +41,7 @@ int ok_pause;
 
 void setup()
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
   Keyboard.begin();
 }
@@ -53,59 +53,55 @@ void loop() {
   if (irrecv.decode(&results)) {
     
     key_pressed = results.value;
-    Serial.print("pressed ");
-    Serial.print(" ");
-    Serial.println(key_pressed);
-	
-	  if (key_pressed == SONY_IR_PLAY) {
-      Consumer.write(MEDIA_PLAY_PAUSE);
-	    ok_pause = 1;
-    }
-    else if (key_pressed == SONY_IR_PREV) {
-      Consumer.write(MEDIA_VOLUME_UP);
-	    ok = 1;
-    }
-    else if (key_pressed == SONY_IR_FAST_PREV) {
-      Consumer.write(MEDIA_VOLUME_DOWN);
-	    ok = 1;
-    }
-    else if (key_pressed == SONY_IR_PAUSE) {
-      Consumer.write(MEDIA_RECORD);
-      ok_pause = 1;
-    }
-    else if (key_pressed == SONY_IR_NEXT) {
-      Keyboard.write(KEY_LEFT_WINDOWS);
-      ok = 1;
-    }
-    else if (key_pressed == SONY_IR_FAST_NEXT) {
-      Keyboard.write(KEY_TAB);
-      Keyboard.write(KEY_TAB);
-      delay(50);
-      Keyboard.write(KEY_DOWN_ARROW);
-      Keyboard.write(KEY_DOWN_ARROW);
-      Keyboard.write(KEY_DOWN_ARROW);
-      Keyboard.write(KEY_DOWN_ARROW);
-      ok = 1;
-    }
-    else if (key_pressed == SONY_IR_UP) {
-      Keyboard.write(KEY_UP_ARROW);
-      ok = 1;
-    }
-    else if (key_pressed == SONY_IR_DOWN) {
-      Keyboard.write(KEY_DOWN_ARROW);
-      ok = 1;
-    }
-    else if (key_pressed == SONY_IR_LEFT) {
-      Keyboard.write(KEY_LEFT_ARROW);
-      ok = 1;
-    }
-    else if (key_pressed == SONY_IR_RIGHT) {
-      Keyboard.write(KEY_RIGHT_ARROW);
-      ok = 1;
-    }
-    else if (key_pressed == SONY_IR_OK) {
-      Keyboard.write(KEY_ENTER);
-      ok = 1;
+    //Serial.print("pressed ");
+    //Serial.print(" ");
+    //Serial.println(key_pressed);
+
+    ok = 1;
+    switch(key_pressed) {
+      case SONY_IR_PLAY:
+        Consumer.write(MEDIA_PLAY_PAUSE);
+        ok = 0;
+        ok_pause = 1;
+        break;
+      case SONY_IR_PAUSE:
+        Consumer.write(MEDIA_RECORD);
+        ok = 0;
+        ok_pause = 1;
+        break;
+      case SONY_IR_PREV:
+        Consumer.write(MEDIA_VOLUME_UP);
+        break;
+      case SONY_IR_FAST_PREV:
+        Consumer.write(MEDIA_VOLUME_DOWN);
+        break;
+      case SONY_IR_NEXT:
+        Keyboard.write(KEY_LEFT_WINDOWS);
+        break;
+      case SONY_IR_FAST_NEXT:
+        Keyboard.write(KEY_TAB);
+        Keyboard.write(KEY_TAB);
+        delay(50);
+        Keyboard.write(KEY_DOWN_ARROW);
+        Keyboard.write(KEY_DOWN_ARROW);
+        Keyboard.write(KEY_DOWN_ARROW);
+        Keyboard.write(KEY_DOWN_ARROW);
+        break;
+      case SONY_IR_UP:
+        Keyboard.write(KEY_UP_ARROW);
+        break;
+      case SONY_IR_DOWN:
+        Keyboard.write(KEY_DOWN_ARROW);
+        break;
+      case SONY_IR_LEFT:
+        Keyboard.write(KEY_LEFT_ARROW);
+        break;
+      case SONY_IR_RIGHT:
+        Keyboard.write(KEY_RIGHT_ARROW);
+        break;
+      case SONY_IR_OK:
+        Keyboard.write(KEY_ENTER);
+        break;
     }
 	
     if (ok) delay(150);
